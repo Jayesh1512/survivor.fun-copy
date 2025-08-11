c50ea4cd8ef877982fd69dac9c400d0bd31e94b8
rk-rishikesh <rvk.rishikeshkale@gmail.com>
2025-08-11 23:27:01 +0530

    Wallet Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,44 @@
-import './globals.css';
-import type { Metadata, Viewport } from 'next';
-import { Manrope } from 'next/font/google';
-import { getUser, getTeamForUser } from '@/lib/db/queries';
-import { SWRConfig } from 'swr';
+import type { Metadata } from "next";
+import { type ReactNode } from 'react';
+import { Providers } from './providers'; 
 
-export const metadata: Metadata = {
-  title: 'Next.js SaaS Starter',
-  description: 'Get started quickly with Next.js and Postgres.'
-};
 
-export const viewport: Viewport = {
-  maximumScale: 1
-};
 
-const manrope = Manrope({ subsets: ['latin'] });
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
+/**
+ * Metadata for the page
+ */
+export async function generateMetadata(): Promise<Metadata> {
+  const URL = process.env.NEXT_PUBLIC_URL;
+  return {
+    title: "survivor-fun",
+    description:
+      "survivor-fun - minikit",
+    other: {
+      "fc:frame": JSON.stringify({
+        version: "next",
+         imageUrl: process.env.NEXT_PUBLIC_IMAGE_URL,
+        button: {
+          title: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
+          action: {
+            type: "launch_frame",
+            name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+            url: URL,
+            splashImageUrl: process.env.NEXT_PUBLIC_IMAGE_URL,
+            iconUrl: process.env.NEXT_PUBLIC_IMAGE_URL,
+            splashBackgroundColor:
+              "#FFF",
+          },
+        },
+      }),
+    },
+  };
+}
+
+export default function RootLayout(props: { children: ReactNode }) {
   return (
-    <html
-      lang="en"
-      className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
-    >
-      <body className="min-h-[100dvh] bg-gray-50">
-        <SWRConfig
-          value={{
-            fallback: {
-              // We do NOT await here
-              // Only components that read this data will suspend
-              '/api/user': getUser(),
-              '/api/team': getTeamForUser()
-            }
-          }}
-        >
-          {children}
-        </SWRConfig>
-      </body>
-    </html>
+       <Providers>
+        {props.children}
+     </Providers>
   );
 }
---
+++ app/providers.tsx
@@ -0,0 +1,56 @@
+'use client';
+import type { ReactNode } from 'react';
+import { MiniKitProvider } from '@coinbase/onchainkit/minikit';
+import { createAppKit } from '@reown/appkit/react';
+import { WagmiProvider } from 'wagmi';
+import { base } from '@reown/appkit/networks';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi';
+
+const queryClient = new QueryClient();
+const projectId = '48d0600b40c62dbdd017ffb85ad8bf90';
+
+const metadata = {
+  name: 'Survivor.fun',
+  description: 'survivor.fun',
+  url: 'https://survivor.fun', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/179229932']
+};
+
+const networks = [base];
+const wagmiAdapter = new WagmiAdapter({
+  networks,
+  projectId,
+  ssr: true
+});
+
+createAppKit({
+  adapters: [wagmiAdapter],
+  networks,
+  projectId,
+  metadata,
+  features: {
+    analytics: true // Optional - defaults to your Cloud configuration
+  }
+});
+
+
+export function Providers(props: { children: ReactNode }) {
+
+  return (
+    <MiniKitProvider
+      apiKey={process.env.NEXT_PUBLIC_CDP_API_KEY_NAME}
+      chain={base}
+    >
+      <WagmiProvider config={wagmiAdapter.wagmiConfig}>
+        <QueryClientProvider client={queryClient}>
+
+          <html lang="en">
+            <meta name="viewport" content="width=device-width, initial-scale=1, max-scale=1" />
+            {props.children}
+          </html>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </MiniKitProvider>
+  );
+}
