bc45ba26cd5edf46667b566935fd112bbba43943
Harsh Gupta <harsh.gupta2211@gmail.com>
2025-08-23 00:16:45 +0530

    minor fix

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -12,26 +12,12 @@ export async function GET() {
             payload: process.env.FARCASTER_PAYLOAD,
             signature: process.env.FARCASTER_SIGNATURE,
         },
-        frame: withValidProperties({
-            version: '1',
-            name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
-            subtitle: process.env.NEXT_PUBLIC_APP_SUBTITLE,
-            description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
-            screenshotUrls: [],
-            iconUrl: process.env.NEXT_PUBLIC_APP_ICON,
-            splashImageUrl: process.env.NEXT_PUBLIC_APP_SPLASH_IMAGE,
-            splashBackgroundColor: process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
-            homeUrl: URL,
-            webhookUrl: `${URL}/api/webhook`,
-            primaryCategory: process.env.NEXT_PUBLIC_APP_PRIMARY_CATEGORY,
-            tags: [],
-            heroImageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
-            tagline: process.env.NEXT_PUBLIC_APP_TAGLINE,
-            ogTitle: process.env.NEXT_PUBLIC_APP_OG_TITLE,
-            ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
-            ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
-            // use only while testing
-            noindex: process.env.ENVIRONMENT === "production" ? "false" : "true",
-        }),
+        "frame": {
+            "version": "1",
+            "name": "survivor.fun",
+            "homeUrl": "https://survivor-fun.vercel.app",
+            "webhookUrl": "https://survivor-fun.vercel.app/api/webhook",
+            "noindex": process.env.ENVIRONMENT === "production" ? "false" : "true"
+        }
     });
 }
